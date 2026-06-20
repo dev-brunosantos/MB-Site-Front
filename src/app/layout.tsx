53b99baea8cecdff1319fe7ed2e68fe6593297da
Bruno Santos <brunosdesenvolvedor@gmail.com>
2026-06-20 15:16:34 -0300

    Adicionando o provider principal da aplicação ao layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { MenuComponent } from "../components/menu";
+import { AppProvider } from "../contexts/AppContexts";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -29,8 +30,10 @@ export default function RootLayout({
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <body className="min-h-full ">
-        <MenuComponent />
-        {children}
+        <AppProvider>
+          <MenuComponent />
+          {children}
+        </AppProvider>
       </body>
     </html>
   );
