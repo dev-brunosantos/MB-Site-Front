ed027808bd2f4a9a0f124dda17a542006307d5dc
Bruno Santos <brunosdesenvolvedor@gmail.com>
2026-06-20 16:22:48 -0300

    Melhorando as dimensões da tela de Perfil

--- src/app/aluno/perfil/page.tsx
+++ src/app/aluno/perfil/page.tsx
@@ -13,15 +13,15 @@ export default function Perfil() {
     return (
         <div className="w-full min-h-screen bg-white text-slate-100 font-sans selection:bg-blue-600 overflow-x-hidden">
 
-            <MenuAluno />
+            <div className="w-full h-15 flex items-center justify-between fixed top-0 left-0 z-50 bg-white">
+                <TituloInfor
+                    titulo="Meu Perfil"
+                    subTitulo="Gerencie suas preferências de conta"
+                />
+            </div>
 
-            <main className="w-full max-w-md mx-auto pt-20 pb-16 px-6 relative z-10 flex flex-col gap-16">
+            <main className="w-full max-w-md mx-auto pt-25 pb-16 px-6 relative z-10 flex flex-col gap-16">
                 <div className="flex flex-col gap-6 animate-in fade-in duration-200">
-
-                    <TituloInfor 
-                        titulo="Meu Perfil" 
-                        subTitulo="Gerencie suas preferências de conta" 
-                    />
 
                     <div className="flex items-center gap-4 p-4 border border-slate-100 rounded-2xl bg-white shadow-sm">
                         <div className="w-14 h-14 rounded-full bg-[#0052e0] flex items-center justify-center text-white text-lg font-bold">
@@ -40,9 +40,9 @@ export default function Perfil() {
                         <button className="w-full py-3 px-4 bg-white border border-slate-100 rounded-xl text-left text-sm font-bold text-slate-700 hover:bg-slate-50">
                             Assinatura e Cobrança
                         </button>
-                      
 
-                        <Button 
+
+                        <Button
                             variant="destructive"
                             className="h-11 px-6 py-2.5 rounded-lg text-sm tracking-wide font-semibold shadow-lg "
                             onClick={logout}
